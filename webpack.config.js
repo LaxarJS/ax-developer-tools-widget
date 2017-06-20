117443155b3ca3f84c19be6bfd61d347ef984075
Jonas Schulte <jonas.schulte@aixigo.de>
2017-06-20 14:28:54 +0200

    (#65) upgraded to LaxarJS v2

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015-2017 aixigo AG
+ * Released under the MIT license.
+ * http://laxarjs.org/license
+ */
+/* eslint-env node */
+
+const pkg = require( './package.json' );
+const path = require( 'path' );
+
+const webpack = require( 'laxar-infrastructure' ).webpack( {
+   context: __dirname,
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: path.resolve( __dirname, 'node_modules' ),
+            loader: 'babel-loader'
+         },
+         {
+            test: /\.spec.js$/,
+            exclude: path.resolve( __dirname, 'node_modules' ),
+            loader: 'laxar-mocks/spec-loader'
+         }
+      ]
+   }
+} );
+
+
+module.exports = [
+   webpack.library(),
+   webpack.browserSpec( [ `./spec/${pkg.name}.spec.js` ] )
+];
